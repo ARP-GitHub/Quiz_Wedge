3b77c9d8babd398be1031172c4b3c09b70ec29bc
Roberto Pastor <wedge.hero@gmail.com>
2015-06-09 11:12:51 +0200

    Versión 11: Creación de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -31,8 +31,6 @@ exports.index = function(req, res) {
         res.render('quizes/index', {quizes:quizes});
       }).catch(function(error){ next(error); });
   }
-
-
 };
 
 // GET /quizes/:id
@@ -47,4 +45,23 @@ exports.answer = function(req, res){
     resultado = 'Correcto';
   }
   res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+};
+
+// GET /quizes/new
+exports.new = function(req,res){
+  var quiz = models.Quiz.build(
+    {pregunta: "Pregunta", respuesta: "Respuesta"}
+  );
+
+  res.render('quizes/new', {quiz: quiz});
+};
+
+// POST /quizes/create
+exports.create = function(req, res){
+  var quiz = models.Quiz.build(req.body.quiz);
+
+  // Guarda en DB los campos prguntas y respuesta de quiz
+  quiz.save({fields:["pregunta", "respuesta"]}).then(function(){
+    res.redirect('/quizes'); 
+  });
 };
